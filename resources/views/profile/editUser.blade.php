baebdccf6c65c2b13f0761612466928a8c3df47d
Jesus Carlos <contacto0carlos@gmail.com>
2023-03-30 18:25:11 -0700

    fix edit user title

--- resources/views/profile/editUser.blade.php
+++ resources/views/profile/editUser.blade.php
@@ -7,7 +7,7 @@
 @section('section')
     <div class="mt-4 mb-4 d-flex justify-content-center align-items-center">
         <div class="col-md-4 p-5 shadow-sm border rounded-3">
-            <h2 class="text-center mb-4 text-primary">Editar cuenta</h2>
+            <h2 class="text-center mb-4 text-primary">Editar Perfil</h2>
             <form>
                 <div class="mb-3">
                     <label for="inputName" class="form-label">Nombre</label>
@@ -18,7 +18,7 @@
                     <input type="email" class="form-control border border-primary" id="inputEmail">
                 </div>
                 <div class="d-grid">
-                    <button class="btn btn-primary" type="submit">Editar perfil</button>
+                    <button class="btn btn-primary" type="submit">Editar</button>
                 </div>
             </form>
         </div>
